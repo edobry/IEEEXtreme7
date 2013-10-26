59782e8a0b4b36eedfb1bb0a039f28d5c2b95e7c
Rob Kellett <rob@aorist.co>
2013-10-25 20:02:52 -0400

    HERE WE GO

---
+++ Solution.cs
@@ -0,0 +1,21 @@
+﻿// Rob Kellett, team CRLF
+// IEEEXtreme 7.0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace IEEE
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey(); // Keep the console open
+        }
+    }
+}
